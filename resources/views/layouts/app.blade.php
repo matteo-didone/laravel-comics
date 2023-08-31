ca3121a45de3c4ce6d18dd04b1ce8113c223c31a
matteo-didone <matteo.dido9@gmail.com>
2023-08-31 18:12:07 +0200

    Generated key

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Comics</title>
+</head>
+
+<body>
+    <h1>This is the default Laravel view</h1>
+</body>
+</html>
